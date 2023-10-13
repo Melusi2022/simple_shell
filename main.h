43aec8f7bffbc965386b03614c5fcd9ed70194df
Nothando Duma <fai231kalwandle@gmail.com>
2023-10-13 06:40:55 -0700

    Removed unused files

--- main.h
+++ main.h
@@ -135,14 +135,4 @@ int _putchar(char c);
 int access_check(char **arg, char *cmd, char *err, int c, char **e);
 void _perrore(char *err, int count, char *c, char *arg);
 
-
-#define DELIM " \n"
-extern char **environ;
-
-char *read_line(void);
-void print_prompt(void);
-char **parse_str(char *str);
-void exec_cmds(char **cmd, char **env);
-char *find_path(char **cmd);
-
 #endif
